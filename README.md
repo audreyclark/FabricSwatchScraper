dc73ed2235af9989abb6344a4a3d3eceb82e7ffa
Audrey Clark <audreyclark@mac.com>
2023-11-16 21:40:46 +0000

    Updated requirements to properly set up env. Updated scraper to handle multiple websites. Next: scraping Moda

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 Scrapes the sites of the three main providers of solid fabric:
  - Andover Fabrics (done)
  - Kona Cotton (Robert Kaufman) (not started)
- - Bella Solids (Moda Fabrics) (not started)
+ - Bella and Sandy Solids (Moda Fabrics) (not started)
--- scraper.py
+++ scraper.py
@@ -7,47 +7,83 @@ from bs4 import BeautifulSoup
 import pandas as pd
 import json
 
-# initiate data storage
-titles = []
-imgLinks = []
-collectionNames = []
-companyNames = []
 
-# open json file, reading list of urls to parse
-json_file = 'fabricReferences.json'
-with open(json_file) as json_data:
-    fabricRefs = json.load(json_data)
-    # for each of the urls in get fabric swatch elements on page
-    for ref in fabricRefs:
-        baseURL = ref["BaseURL"]
-        url = ref["swatchListURL"]
-        headers = {"Accept-Language": "en-US, en;q=0.5"}
-        results = requests.get(url, headers=headers)
+def main():
+    # initiate data storage
+    titles = []
+    imgLinks = []
+    collectionNames = []
+    companyNames = []
 
-        soup = BeautifulSoup(results.text, "html.parser")
+    # open json file, reading list of urls to parse
+    json_file = 'fabricReferences.json'
+    with open(json_file) as json_data:
+        fabricRefs = json.load(json_data)
+        # for each of the urls in get fabric swatch elements on page
+        for ref in fabricRefs:
+            baseURL = ref["BaseURL"]
+            url = ref["swatchListURL"]
+            headers = {"Accept-Language": "en-US, en;q=0.5"}
+            results = requests.get(url, headers=headers)
 
-        swatch_divs = soup.find_all('a', class_='Nivo')
+            soup = BeautifulSoup(results.text, "html.parser")
 
-        # loop through each element, getting image, title
-        for container in swatch_divs:
-            # name
-            name = container.get('title')
-            titles.append(name)
+            match baseURL:
+                case "https://www.andoverfabrics.com":
+                    returnedArray = handleAndover(soup, baseURL)
+                    titles.extend(returnedArray[0])
+                    imgLinks.extend(returnedArray[1])
+                    for t in returnedArray[0]:
+                        collectionNames.append(ref["groupName"])
+                        companyNames.append(ref["companyName"])
+                case "https://modafabrics.com/":
+                    handleModa(soup, baseURL)
+                case "https://www.robertkaufman.com/":
+                    handleRK(soup, baseURL)
+                case _:
+                    print("ERROR: baseURL didn't match expected. BaseURL: " + baseURL)
 
-            # imgLink
-            cleanedLink = container.get('href').replace('..', baseURL)
-            imgLinks.append(cleanedLink)
+    print("Length of found arrays:")
+    print("Titles: " + str(len(titles)))
+    print("imgLinks: " + str(len(imgLinks)))
+    print("collectionNames: " + str(len(collectionNames)))
+    print("companyNames: " + str(len(companyNames)))
+    # create dataframe to reference in csv
+    swatches = pd.DataFrame({
+        'Swatch Name': titles,
+        'Image Link': imgLinks,
+        'Collection Name': collectionNames,
+        'Company': companyNames
+    })
+    # output to csv
+    swatches.to_csv('swatches.csv')
 
-            collectionNames.append(ref["groupName"])
-            companyNames.append(ref["companyName"])
 
-# create dataframe to reference in csv
-swatches = pd.DataFrame({
-    'Swatch Name': titles,
-    'Image Link': imgLinks,
-    'Collection Name': collectionNames,
-    'Company': companyNames
-})
+# Andover fabrics has you pull the images from their site
+def handleAndover(soup, baseURL):
+    tempTitles = []
+    tempImgLinks = []
+    swatch_divs = soup.find_all('a', class_='Nivo')
 
-# output to csv
-swatches.to_csv('swatches.csv')
+    # loop through each element, getting image, title
+    for container in swatch_divs:
+        # name
+        name = container.get('title')
+        tempTitles.append(name)
+
+        # imgLink
+        cleanedLink = container.get('href').replace('..', baseURL)
+        tempImgLinks.append(cleanedLink)
+    return ([tempTitles, tempImgLinks])
+
+
+# Moda fabrics lets you download a zip file of images
+def handleModa(soup, baseURL):
+    print("Todo: Handle Moda")
+
+
+def handleRK(soup, baseURL):
+    print("Todo: Handle RK")
+
+
+main()
